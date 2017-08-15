32f73cd9c5c6ed5fb702ad0dd2ffe6e8116bb9b0
knutole <knutole@mapic.io>
2017-08-15 11:07:27 +0000

    cleanup

--- start-server.sh
+++ start-server.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# # source config
-# source /mapic/config/env.sh || exit 1
-
-# # TODO: find a better way ?
-# export SYSTEMAPIC_PGSQL_USERNAME \
-#        SYSTEMAPIC_PGSQL_PASSWORD \
-#        SYSTEMAPIC_PGSQL_DBNAME
-
 # ensure log folder
 mkdir -p /mapic/mile/log
 
@@ -15,17 +7,8 @@ mkdir -p /mapic/mile/log
 yarn config set cache-folder /mapic/mile/.yarn
 yarn install
 
-# ensure 
+# go to folder
 cd /mapic/mile
 
 # spin server
-# if $MAPIC_PRODMODE; then
-# 	echo 'Mile | PostGIS Tile Server | Production mode'
-# 	forever src/mile.js production >> log/mile.log
-# else
-# 	echo 'Mile Debug mode (with 8GB memory)'
-# 	nodemon --max-old-space-size=8192 -i node_modules/ -i test/ src/mile.js
-# fi
-echo "forever + nodemon"
-# forever -c nodemon --exitcrash --max-old-space-size=8192 -i node_modules/ -i test/ src/mile.js
 forever -f -w --watchDirectory src/ src/mile.js
