fb3e2f236ac59d5dbe06fd9a60720b3ccf782598
knutole <knutole@mapic.io>
2017-08-19 13:28:08 +0000

    pull code from git

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# get code
+MILE_BRANCH=${MAPIC_MILE_BRANCH:-master}
+echo "MILE_BRANCH: $MILE_BRANCH"
+mkdir -p /mapic && cd /mapic
+git clone https://github.com/mapic/mile.git
+cd /mapic/mile
+git checkout MILE_BRANCH
+
 # ensure log folder
 mkdir -p /mapic/mile/log
 
@@ -7,11 +15,8 @@ mkdir -p /mapic/mile/log
 yarn config set cache-folder /mapic/mile/.yarn
 yarn install
 
-# go to folder
-cd /mapic/mile
-
 # clean up forever
 rm -rf /root/.forever
 
 # spin server
-forever -f -v -w --watchDirectory src/ src/mile.js
+forever -m 100 --spinSleepTime 1000 -f -v -w --watchDirectory src/ src/mile.js
