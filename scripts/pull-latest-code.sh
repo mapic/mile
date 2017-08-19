ec4674743d39c416fe7e7debda2d3f5d4d56ccaa
knutole <knutole@mapic.io>
2017-08-19 14:00:13 +0000

    update code from git

--- scripts/pull-latest-code.sh
+++ scripts/pull-latest-code.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# add key
-# touch ~/.ssh/known_hosts
-# ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-echo "PWD1: $PWD"
-cd /mapic/mile
-echo "PWD2: $PWD"
 git remote set-url origin https://github.com/mapic/mile.git
 git pull origin master
 git checkout ${MAPIC_MILE_BRANCH:-master}
