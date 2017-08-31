095ae56aa17fba908b8855607db69fa97be7e8b4
knutole <knutole@mapic.io>
2017-08-31 15:39:52 +0000

    print branch

--- scripts/pull-latest-code.sh
+++ scripts/pull-latest-code.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+BRANCH=${MAPIC_MILE_BRANCH:-master}
+echo "Checking out $BRANCH"
 git remote set-url origin https://github.com/mapic/mile.git
 git pull origin master
-git checkout ${MAPIC_MILE_BRANCH:-master}
+git checkout $BRANCH
 git remote set-url origin git@github.com:mapic/mile.git
