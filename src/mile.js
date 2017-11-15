7fdc3ac4c763f7e6fdfdf69a1a48fbf5e3989d4b
knutole <knutolesjoli@gmail.com>
2017-11-15 02:51:22 +0100

    cleanup

--- src/mile.js
+++ src/mile.js
@@ -23,6 +23,7 @@ var geojsonArea = require('geojson-area');
 var crypto = require('crypto');
 var https = require('https');
 var geojsonExtent = require('geojson-extent');
+
 var turf = {};
 turf.booleanOverlap = require('@turf/boolean-overlap');
 turf.bboxPolygon = require('@turf/bbox-polygon');
@@ -1306,12 +1307,7 @@ module.exports = mile = {
             params : params, 
             layer : storedLayer
         });
-        if (outside_extent) {
-            // console.log('outside extent (raster):', outside_extent);
-            return done('outside extent');
-        } else {
-            // console.log('outside_extent (raster):', outside_extent)
-        }
+        if (outside_extent) return done('outside extent');
 
         // check cache
         store._readRasterTile(params, function (err, data) {
@@ -1350,10 +1346,8 @@ module.exports = mile = {
             params : params, 
             layer : storedLayer
         });
-        if (outside_extent) {
-            // console.log('outside extent - grid');
-            return done('outside extent');
-        }
+        if (outside_extent) return done('outside extent');
+
         // check cache
         store.getGridTile(params, function (err, data) {
 
@@ -1418,71 +1412,6 @@ module.exports = mile = {
     checkAccess : tools.checkAccess,
 }
 
-
-// http://wiki.openstreetmap.org/wiki/Mercator#JavaScript
-var Conv=({
-    r_major : 6378137.0,//Equatorial Radius, WGS84
-    r_minor : 6356752.314245179,//defined as constant
-    f : 298.257223563,//1/f=(a-b)/a , a=r_major, b=r_minor
-    deg2rad : function(d) {
-        var r=d*(Math.PI/180.0);
-        return r;
-    },
-    rad2deg : function(r) {
-        var d=r/(Math.PI/180.0);
-        return d;
-    },
-    ll2m : function(lon,lat) { //lat lon to mercator
-    
-        //lat, lon in rad
-        var x=this.r_major * this.deg2rad(lon);
-        if (lat > 89.5) lat = 89.5;
-        if (lat < -89.5) lat = -89.5;
-        var temp = this.r_minor / this.r_major;
-        var es = 1.0 - (temp * temp);
-        var eccent = Math.sqrt(es);
-        var phi = this.deg2rad(lat);
-        var sinphi = Math.sin(phi);
-        var con = eccent * sinphi;
-        var com = .5 * eccent;
-        var con2 = Math.pow((1.0-con)/(1.0+con), com);
-        var ts = Math.tan(.5 * (Math.PI*0.5 - phi))/con2;
-        var y = 0 - this.r_major * Math.log(ts);
-        var ret={'x':x,'y':y};
-        return ret;
-    },
-    m2ll : function(x,y) {//mercator to lat lon
-        var lon=this.rad2deg((x/this.r_major));
-        var temp = this.r_minor / this.r_major;
-        var e = Math.sqrt(1.0 - (temp * temp));
-        var lat=this.rad2deg(this.pj_phi2( Math.exp( 0-(y/this.r_major)), e));
-        var ret={'lon':lon,'lat':lat};
-        return ret;
-    },
-    pj_phi2 : function(ts, e) {
-        var N_ITER=15;
-        var HALFPI=Math.PI/2;
-        var TOL=0.0000000001;
-        var eccnth, Phi, con, dphi;
-        var i;
-        var eccnth = .5 * e;
-        Phi = HALFPI - 2. * Math.atan (ts);
-        i = N_ITER;
-        do 
-        {
-            con = e * Math.sin (Phi);
-            dphi = HALFPI - 2. * Math.atan (ts * Math.pow((1. - con) / (1. + con), eccnth)) - Phi;
-            Phi += dphi;
-        } 
-        while ( Math.abs(dphi)>TOL && --i);
-        return Phi;
-    }
-});
-
 // start server
 // todo: move around
 server(mile);
-
-
-
-
